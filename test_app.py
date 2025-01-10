e3e58e0ca3c41ba870293b28297187b05b2fddd9
nasir015 <nasir.uddin.6314@gmail.com>
2025-01-10 17:20:46 +0600

    add all code in file

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
+
+def test_home():
+    response=app.test_client().get("/")
+
+    assert response.status_code==200
+    assert response.data== b"Hello World!"
